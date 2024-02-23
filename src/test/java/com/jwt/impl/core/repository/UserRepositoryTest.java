cc43be1aa508ad961625301bec89aec35171d407
Ezzerof <christian.bitca@gmail.com>
2024-02-23 15:33:01 +0000

    Created first User repository test

--- src/test/java/com/jwt/impl/core/repository/UserRepositoryTest.java
+++ src/test/java/com/jwt/impl/core/repository/UserRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.jwt.impl.core.repository;
 
-import com.jwt.impl.core.persistance.entity.Task;
 import com.jwt.impl.core.persistance.entity.User;
 import com.jwt.impl.core.persistance.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -9,26 +8,32 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserRepositoryTest {
+
     @Autowired
     private UserRepository userRepository;
 
     @Test
     public void saveUser_ReturnUser() {
-//        User user = new User(
-//                "Username",
-//                "Adrian",
-//                "Dan",
-//                "Surname",
-//                23,
-//                "email@gmail.com",
-//                "passworD123!",
-//                "Course",
-//                "1234567898"
-//        );
+        User user = new User(
+                "Username",
+                "Adrian",
+                "Dan",
+                "Surname",
+                23,
+                "email@gmail.com",
+                "passworD123!",
+                "Course",
+                "1234567898"
+        );
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
     }
 }
