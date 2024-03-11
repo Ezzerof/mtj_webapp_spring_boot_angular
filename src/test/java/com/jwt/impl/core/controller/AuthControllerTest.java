021846c6dc6f0b18245c9a8478479830ae7cdae4
Ezzerof <christian.bitca@gmail.com>
2024-03-11 18:56:52 +0000

    Performed tests for AuthController and AuthService

--- src/test/java/com/jwt/impl/core/controller/AuthControllerTest.java
+++ src/test/java/com/jwt/impl/core/controller/AuthControllerTest.java
@@ -2,7 +2,10 @@ package com.jwt.impl.core.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jwt.impl.core.persistance.entity.User;
+import com.jwt.impl.core.service.AuthenticationService;
 import com.jwt.impl.core.service.AuthenticationServiceImpl;
+import com.jwt.impl.core.service.TaskReminderService;
+import com.jwt.impl.rest.AuthController;
 import com.jwt.impl.rest.payload.request.ChangePasswordRequest;
 import com.jwt.impl.rest.payload.request.SignInRequest;
 import com.jwt.impl.rest.payload.request.SignUpRequest;
@@ -16,32 +19,36 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = AuthControllerTest.class)
+
+@WebMvcTest(controllers = AuthController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private AuthenticationServiceImpl service;
+    private AuthenticationService service;
     @MockBean
     private JwtService jwtService;
+    @MockBean
+    private TaskReminderService taskReminderService;
     @Autowired
     private ObjectMapper objectMapper;
-
     private User user;
     private SignInRequest signInRequest;
     private SignUpRequest signUpRequest;
+    private Authentication authentication;
     private ChangePasswordRequest changePasswordRequest;
 
     @BeforeEach
@@ -69,6 +76,11 @@ public class AuthControllerTest {
                 "Course",
                 "1234567898"
         );
+        changePasswordRequest = new ChangePasswordRequest(
+                "oldPassword",
+                "newPassword",
+                "admin@gmail.com"
+        );
     }
 
     @Test
@@ -78,18 +90,63 @@ public class AuthControllerTest {
         mockMvc.perform(post("/api/v1/auth/sign-up")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(signUpRequest)))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
     }
 
     @Test
     public void AuthController_Authenticate_ReturnUser() throws Exception {
+        String generatedToken = "token";
         when(service.authenticate(any(SignInRequest.class))).thenReturn(user);
-        when(jwtService.generateToken(anyString())).thenReturn("token");
+        when(jwtService.generateToken(anyString())).thenReturn(generatedToken);
 
-        ResultActions response = mockMvc.perform(post("/api/v1/auth/sign-in")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signInRequest)));
+        mockMvc.perform(post("/api/v1/auth/sign-in")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signInRequest)))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.token").value(generatedToken));
 
-        response.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void AuthController_DeleteAccount_AccountSuccessfullyDeleted() throws Exception {
+        when(service.deleteAccount(authentication)).thenReturn(true);
+
+        mockMvc.perform(delete("/api/v1/auth/delete-account")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.message").value("Account successfully deleted."));
+    }
+
+    @Test
+    public void AuthController_DeleteAccount_AccountNotFound() throws Exception {
+        when(service.deleteAccount(authentication)).thenReturn(false);
+
+        mockMvc.perform(delete("/api/v1/auth/delete-account")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isNotFound())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.error").value("Account not found."));
+    }
+
+    @Test
+    public void AuthController_ChangePassword_200() throws Exception {
+        when(service.changePassword(any(ChangePasswordRequest.class))).thenReturn(true);
+
+        mockMvc.perform(post("/api/v1/auth/change-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(changePasswordRequest)))
+                        .andExpect(status().isOk());
+    }
+
+    @Test
+    public void AuthController_ChangePassword_400() throws Exception {
+        when(service.changePassword(any(ChangePasswordRequest.class))).thenReturn(false);
+
+        mockMvc.perform(post("/api/v1/auth/change-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(changePasswordRequest)))
+                        .andExpect(status().isBadRequest());
     }
 }
